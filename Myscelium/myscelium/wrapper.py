3439c32ce883dce9ae32c6b88096a91837e97f90
Cristian Camargo <ccf@cdone.com.br>
2023-08-03 22:57:32 -0300

    Add multi threading option to logs clback transposition and add dockstrings in the entire wrapper!

--- Myscelium/myscelium/wrapper.py
+++ Myscelium/myscelium/wrapper.py
@@ -13,6 +13,13 @@ import time
 class MysceliumHostInterface:
 
     def __init__(self, buffer_path:str) -> None:
+
+        """
+        Initialize the MysceliumHostInterface.
+
+        Parameters:
+        - buffer_path: Path to the buffer for logs retrieval.
+        """
         
         self.logs_retriver = logs_retriver.Logs_Buffer_Retriver(buffer_path)
 
@@ -22,9 +29,67 @@ class MysceliumHostInterface:
 
         self.process
 
+        self.transposition_threads = 1
+
         pass
 
     def retrive_logs (self):
+
+        """
+        Retrieve logs and process them. If multiple threads are set, it will split the logs 
+        and process them in parallel.
+        """
+
+        def split_dataframe(df, num_chunks):
+            
+            """
+            Split a DataFrame into num_chunks parts.
+            
+            If the DataFrame cannot be split exactly into num_chunks, the remainder will be 
+            distributed among the chunks.
+            
+            Parameters:
+            - df: DataFrame to be split
+            - num_chunks: Number of chunks
+            
+            Returns:
+            - List of DataFrames
+            """
+
+            n = len(df)
+            chunk_size = n // num_chunks
+            remainder = n % num_chunks
+            
+            chunks = []
+            start = 0
+            for i in range(num_chunks):
+                end = start + chunk_size
+                # Distribute the remainder across the initial chunks
+                if remainder:
+                    end += 1
+                    remainder -= 1
+                chunks.append(df.iloc[start:end])
+                start = end
+            
+            return chunks
+
+        def transpose (logs_df):
+
+            for i in logs_df.index:
+
+                log_id          = logs_df.loc[i, 'ID']
+                log_time        = logs_df.loc[i, 'LogTime']         
+                log_from_node   = logs_df.loc[i, 'NodeName']         
+                log_level       = logs_df.loc[i, 'LogLevel']         
+                log_msg         = logs_df.loc[i, 'LogMsg']         
+
+                self.log_callback(log_time, log_level, log_from_node, log_msg)
+
+                self.logs_retriver.Remove_Log(log_id)
+                
+                continue
+
+            return
 
         while self.stats:
 
@@ -40,31 +105,68 @@ class MysceliumHostInterface:
             logs_df = logs_df.sort_values('LogTime')
             logs_df = logs_df.reset_index(drop=True)
 
-            for i in logs_df.index:
-
-                log_id          = logs_df.loc[i, 'ID']
-                log_time        = logs_df.loc[i, 'LogTime']         
-                log_from_node   = logs_df.loc[i, 'NodeName']         
-                log_level       = logs_df.loc[i, 'LogLevel']         
-                log_msg         = logs_df.loc[i, 'LogMsg']         
-
-                self.log_callback(log_time, log_level, log_from_node, log_msg)
-
-                self.logs_retriver.Remove_Log(log_id)
+            if self.transposition_threads > 1:
                 
-                continue
+                logs_df_chunks = split_dataframe(logs_df, self.transposition_threads)
+                
+                threads = []
+                
+                for chunk in logs_df_chunks:
+                    threads.append(Process(target=self.transpose, args=(chunk)))
+                    continue
+                
+                for t in threads:
+                    t.start()
+                    continue
+                
+                for t in threads:
+                    t.join()
+                    continue
+
+                pass
+            
+            else:
+
+                transpose(logs_df)
+                pass
             
             time.sleep(5)
 
             continue
             
         return
+    
+    def active_multi_handlers (self, threads_num=2):
+
+        """
+        Activate multiple handlers for processing logs.
+
+        Parameters:
+        - threads_num: Number of threads to be used for processing logs.
+        """
+
+        self.transposition_threads = threads_num
+
+        return
 
     def set_logs_callback (self, callback:str):
+
+        """
+        Set the callback function for logs.
+
+        Parameters:
+        - callback: Callback function to be invoked for each log.
+        """
+
         self.log_callback = callback
+
         pass
 
     def stop_logs_reriver (self):
+
+        """
+        Stop the logs retriever process.
+        """
         
         self.stats = False
         self.process.join()
@@ -72,6 +174,10 @@ class MysceliumHostInterface:
         return
 
     def start_logs_retriver (self):
+
+        """
+        Start the logs retriever process in a separate process.
+        """
 
         self.stats = True
 
@@ -82,7 +188,26 @@ class MysceliumHostInterface:
 
 class MysceliumHost:
 
-    def __init__(self, callbacks:list, host_id:int, allowed_clients:list, buffer_path:str, n_workers=2, n_max_conns:int=5, log_level:str="WARN") -> None:
+    def __init__(self, callbacks:list, host_id:int, allowed_clients:list, buffer_path:str, n_workers=2, n_max_conns:int=5, log_level:str="") -> None:
+
+        """
+        Initialize the MysceliumHost.
+
+        Parameters:
+        - callbacks: List of callback functions.
+        - host_id: Unique identifier for the host.
+        - allowed_clients: List of clients allowed to connect.
+        - buffer_path: Path to the buffer.
+        - n_workers: Number of workers.
+        - n_max_conns: Maximum number of connections.
+        - log_level: Logging level.
+
+        Obs:
+        - If you don't se loggin level it will be deactivated by default!
+
+        """ 
+
+        self.logging_level = log_level
 
         self.host_interface = MysceliumHostInterface(buffer_path)
 
@@ -98,7 +223,7 @@ class MysceliumHost:
         
         callbacks = callbacks + special_functions
 
-        if log_level not in ["DEBUG", "INFO", "WARN", "EXCEPTION"]:
+        if log_level not in ["DEBUG", "INFO", "WARN", "EXCEPTION", ""]:
             raise f"Log must be some of this: ('DEBUG', 'INFO', 'WARN', 'EXCEPTION') log level cant be: {log_level}"
         else:
             pass
@@ -115,44 +240,134 @@ class MysceliumHost:
 
         pass
 
-    def set_logs_callback_handler (self, logs_handler_callback:object):
+    def set_logs_callback_handler (self, logs_handler_callback:object, active_multi_handlers:str=False, workers_num:str=2) -> None:
+
+        """
+        Set the logs callback handler.
+
+        Parameters:
+        - logs_handler_callback: Callback function to handle logs.
+        - active_multi_handlers: Flag to activate multiple handlers.
+        - workers_num: Number of workers for handling logs.
+        """
+
+        if self.logging_level == "":
+            raise "To use logging you need to set a loggin level, the current logging status is deactivated!"
+        else:
+            pass
+
+        if active_multi_handlers:
+            self.host_interface.active_multi_handlers(workers_num)
+        else:
+            pass
+
         self.host_interface.set_logs_callback(logs_handler_callback)
+        
         return
     
     # def set_client_heartbeat_handler (self, callback): #! THIS WILL NOT WORK UNTILL PYTHON POOL IS FINISHED
     #     mys.registry_socket_host_client_heartbeat_contact_callback(callback)
     
     def get_registred_commands (self) -> dict:
+
+        """
+        Retrieve the registered commands.
+
+        Returns:
+        - Dictionary of registered commands.
+        """
+
         print("Activated the get registred commands")
+
         return mys.get_socket_host_available_commands()
 
     def initialize_host (self, ip:str, port:int):
-        
-        self.host_interface.start_logs_retriver()
+
+        """
+        Initialize the host with the given IP and port.
+
+        Parameters:
+        - ip: IP address for the host.
+        - port: Port number for the host.
+        """
+
+        if self.logging_level != "":
+            self.host_interface.start_logs_retriver()
+        else:
+            pass
+
         mys.initialize_socket_host (ip, port, self.host_id)
         
         return
 
     def stop_host (self, signal, frame):
-        # This function will be called when a SIGINT signal is received
+
+        """
+        Stop the host. This function is intended to be called when a termination signal is received.
+
+        Parameters:
+        - signal: Signal received.
+        - frame: Current stack frame.
+        """
         
+        # This function will be called when a SIGINT signal is received
+
         mys.stop_socket_host()
-        self.host_interface.stop_logs_reriver()
+
+        if self.logging_level != "":
+            self.host_interface.stop_logs_reriver()
+        else:
+            pass
 
         return
 
     def send (self):
-         pass
+         
+        """
+        Send data. (This method is currently a placeholder and needs to be implemented.)
+        """
+
+        pass
     
 class HostPatterns:
 
     def __init__(self) -> None:
+
+        """
+        Initialize the HostPatterns class.
+        """
+
         pass
 
     def client_pattern (self, client_type:str, client_id:str) -> dict:
+
+        """
+        Create a client pattern.
+
+        Parameters:
+        - client_type: Type of the client.
+        - client_id: Unique identifier for the client.
+
+        Returns:
+        - Dictionary representing the client pattern.
+        """
+
         return {"client_type":client_type, "client_id":client_id}
 
     def response_pattern (self, response:any, response_mode:str, response_activation_function:str = None,  redirect_to_client_id:str=None) -> dict:
+
+        """
+        Create a response pattern.
+
+        Parameters:
+        - response: The actual response data.
+        - response_mode: Mode of the response (e.g., 'redirect' or 'to_origin').
+        - response_activation_function: Activation function for the response.
+        - redirect_to_client_id: Client ID to redirect to (if response_mode is 'redirect').
+
+        Returns:
+        - Dictionary representing the response pattern.
+        """
 
         if response_activation_function == "" or response_activation_function == None:
             raise ("Missing response_activation_function!")
@@ -176,6 +391,17 @@ class HostPatterns:
             raise ("Response mode invalid! Please use one of this: ('redirect', 'to_origin')")
 
     def callback_pattern (self, callback, args) -> dict:
+
+            """
+            Create a callback pattern.
+
+            Parameters:
+            - callback: The callback function.
+            - args: Arguments for the callback function.
+
+            Returns:
+            - Dictionary representing the callback pattern.
+            """
             
             callback_pattern =  {
                 "function": callback,
@@ -190,6 +416,15 @@ class HostPatterns:
 class MysceliumClient:
 
     def __init__(self, client_uid:int, buffer_path:str, log_level:str="WARN") -> None:
+
+        """
+        Initialize the MysceliumClient.
+
+        Parameters:
+        - client_uid: Unique identifier for the client.
+        - buffer_path: Path to the buffer.
+        - log_level: Logging level.
+        """
 
         self.client_uid = client_uid
 
@@ -211,12 +446,39 @@ class MysceliumClient:
     #     mys.registry_client_logs_handler(logs_handler_callback)
 
     def set_client_uid (self, client_uid):
+
+        """
+        Set the client's unique identifier.
+
+        Parameters:
+        - client_uid: Unique identifier for the client.
+        """
+
         mys.set_client_uid(client_uid)
 
+        return 
+
     def set_workers_num (self, n_workers=2):
+
+        """
+        Set the number of workers for the client.
+
+        Parameters:
+        - n_workers: Number of workers.
+        """
+
         mys.set_socket_client_transposer_num_of_workers(n_workers)
 
+        return 
+
     def set_callbacks (self,callbacks:list):
+
+        """
+        Register callback functions for the client.
+
+        Parameters:
+        - callbacks: List of callback functions.
+        """
 
         special_functions = [{
             "function": get_registred_commands,
@@ -228,20 +490,62 @@ class MysceliumClient:
 
         mys.registry_socket_client_callbacks(callbacks) #! We can change this to response handler in the future.
 
+        return 
+
     def get_registred_commands (self) -> dict:
+
+        """
+        Retrieve the registered commands for the client.
+
+        Returns:
+        - Dictionary of registered commands.
+        """
+
         print("Activated the get registred commands")
+
         return mys.get_socket_client_available_commands()
 
     def initialize_client (self, ip:str, port:int):
+
+        """
+        Initialize the client with the given IP and port.
+
+        Parameters:
+        - ip: IP address for the client connect in host.
+        - port: Port number for the client connect in host.
+        """
+
         self.runing = True
         mys.initialize_socket_client (ip, port, self.client_uid)
 
     def stop_client (self, signal, frame):
+
+        """
+        Stop the client. This function is intended to be called when a termination signal is received.
+
+        Parameters:
+        - signal: Signal received.
+        - frame: Current stack frame.
+        """
+
         # This function will be called when a SIGINT signal is received
         mys.stop_socket_client()
 
     def send (self, command:dict, priority:int):
+
+        """
+        Send a command with a specified priority.
+
+        Parameters:
+        - command: The command to be sent.
+        - priority: Priority level of the command.
+
+        Returns:
+        - Response from the send operation.
+        """
+
         print(self.runing)
+
         if not self.runing:
             raise "Client need to be runing before try to send something"
         else:
@@ -251,12 +555,40 @@ class MysceliumClient:
 class ClientPatterns:
 
     def __init__(self) -> None:
+
+        """
+        Initialize the ClientPatterns class.
+        """
+
         pass
 
     def client_pattern (self, client_type:str, client_id:str) -> dict:
+
+        """
+        Create a client pattern.
+
+        Parameters:
+        - client_type: Type of the client.
+        - client_id: Unique identifier for the client.
+
+        Returns:
+        - Dictionary representing the client pattern.
+        """
+
         return {"client_type":client_type, "client_id":client_id}
 
     def command_pattern (self, command_function:str, args=None):
+
+        """
+        Create a command pattern.
+
+        Parameters:
+        - command_function: Function name for the command.
+        - args: Arguments for the command function (default is None).
+
+        Returns:
+        - Dictionary representing the command pattern.
+        """
 
         if args != None:
             return {"function":command_function, "args":args}
@@ -266,6 +598,18 @@ class ClientPatterns:
         return {"function":command_function, "args":""}
 
     def response_pattern (self, response:any, response_mode:str, retransmit_to_client_id:str=None) -> dict:
+
+        """
+        Create a response pattern.
+
+        Parameters:
+        - response: The actual response data.
+        - response_mode: Mode of the response (e.g., 'retransmit' or 'to_host').
+        - retransmit_to_client_id: Client ID to retransmit to (if response_mode is 'retransmit').
+
+        Returns:
+        - Dictionary representing the response pattern.
+        """
 
         if response_mode == "retransmit":
 
@@ -285,12 +629,23 @@ class ClientPatterns:
 
     def callback_pattern (self, callback, args) -> dict:
             
-            callback_pattern =  {
-                "function": callback,
-                "args": args,
-            }
+        """
+        Create a callback pattern.
+
+        Parameters:
+        - callback: The callback function.
+        - args: Arguments for the callback function.
+
+        Returns:
+        - Dictionary representing the callback pattern.
+        """
             
-            return callback_pattern
+        callback_pattern =  {
+            "function": callback,
+            "args": args,
+        }
+        
+        return callback_pattern
 
 # >-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # > FUNCTIONS
@@ -298,6 +653,14 @@ class ClientPatterns:
 host_patterns = HostPatterns()
 
 def get_registred_commands () -> dict:
+
+    """
+    Retrieve the registered commands and format the response.
+
+    Returns:
+    - Dictionary representing the response to be returned to the engine.
+    """
+
     print("Activated the get registred commands")
     response = mys.get_socket_host_available_commands()
 
