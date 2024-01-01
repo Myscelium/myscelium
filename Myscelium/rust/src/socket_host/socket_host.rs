6abd69d4477b2d41e0feeb086c4a3dd63838200e
Cristian Camargo <ccf@cdone.com.br>
2024-01-01 19:12:49 -0300

    Adjust the time to try to sync again

--- Myscelium/rust/src/socket_host/socket_host.rs
+++ Myscelium/rust/src/socket_host/socket_host.rs
@@ -633,7 +633,8 @@ fn handle_connection(mut stream: TcpStream) {
 
                 if let Some(last_sync) = client_last_sync {
                     let current_time = Utc::now();
-                    if current_time - last_sync > Duration::seconds(30) {
+                    if current_time - last_sync > Duration::milliseconds(240) {
+                        // The time to try sinc again needs to be the same time that the db refreshs or multiple of that
                         send_network_available_commands(command.client_key.clone());
 
                         let controller = &CLIENTS_SYNC_CONTROLLER;
