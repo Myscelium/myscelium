c46765558a2238a5a5658fa8d83fd688c2520eec
Cristian Camargo <ccf@cdone.com.br>
2024-01-17 13:42:16 -0300

    Fix the test management for the direct command version

--- Myscelium/tests/test_management/host_module.py
+++ Myscelium/tests/test_management/host_module.py
@@ -117,7 +117,9 @@ class MyHost:
         # TODO >>> In the future change to use 100% timeout
 
         n = 0 
-        COUNTER = 24 # Each counter is 5 secs of waiting
+        # > In default conditions the test only can last 78 seconds (78/5 = 15,6) so round and add +1 for tolerance
+        COUNTER = 17 # Each counter is 5 secs of waiting 
+
 
         mys_host_interface = MysceliumHostInterface("Temp/Data/")
         mys_host_interface.set_client_contact_retriever_callback(client_contact_event_handler)
