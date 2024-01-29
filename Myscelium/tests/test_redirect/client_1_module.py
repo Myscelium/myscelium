45728e20a685578250205cfcb7cd5161a58b3bca
Cristian Camargo <ccf@cdone.com.br>
2024-01-29 13:57:52 -0300

    Add the mechanism to check if the target is wready or not into the test redirect mechanism

--- Myscelium/tests/test_redirect/client_1_module.py
+++ Myscelium/tests/test_redirect/client_1_module.py
@@ -10,54 +10,60 @@ from ..Logs.test_logs_manager import Events_Manager, System_Status
 
 CLIENT_ID = "some_client_id"
 
-class Senders:
 
-    def __init__ (self):
+class Senders:
+    def __init__(self):
         pass
 
     @staticmethod
     def send_some_data():
-
         # time.sleep(10)
-        mys_client = MysceliumClient(client_uid=CLIENT_ID, buffer_path="Temp/Client1Data/")
+        mys_client = MysceliumClient(
+            client_uid=CLIENT_ID, buffer_path="Temp/Client1Data/"
+        )
         mys_client.running = True
+
+        max_attempts = 10
+        attemtps = 0
+        while not mys_client.is_client_ready():
+            time.sleep(1)
+            attemtps += 1
+            if attemtps >= max_attempts:
+                assert False, "Take too long to client be ready"
+            continue
 
         command = client_patterns.command_pattern(
             CLIENT_ID,
-            "python_function", 
-            kwargs={"age": 10, "birth": 8, "name": "cristian"}
+            "python_function",
+            kwargs={"age": 10, "birth": 8, "name": "cristian"},
         )
 
         result = mys_client.send(command, priority=10)
 
         Events_Manager(Unit="Client1", path="Logs").Set_Event(
-            "Data Sended", 
-            event_type="Send", 
-            event_key="1dX2A63Rp7O79x6t"
+            "Data Sended", event_type="Send", event_key="1dX2A63Rp7O79x6t"
         )
 
         print(result)
 
 
 class Receivers:
-
-    def __init__ (self):
+    def __init__(self):
         pass
 
     @staticmethod
-    def test_handler(data:str):
-
+    def test_handler(data: str):
         Events_Manager(Unit="Client1", path="Logs").Set_Event(
-            "Activate Basic Response Test callback handler", 
-            event_type="Receive", 
-            event_key="r99F3i89D20Oj1lq"
+            "Activate Basic Response Test callback handler",
+            event_type="Receive",
+            event_key="r99F3i89D20Oj1lq",
         )
 
         if "status" in data:
             pass
         else:
             return None
-        
+
         if data["status"] == "Success":
             pass
         else:
@@ -66,47 +72,49 @@ class Receivers:
         print("Received data: ", data)
 
         # time.sleep(5)
-        
+
         # System_Status(path="Logs").change_unit_status(Unit="Client1", Status=False)
 
     @staticmethod
-    def test_redirect_handler(data:dict):
-
+    def test_redirect_handler(data: dict):
         Events_Manager(Unit="Client1", path="Logs").Set_Event(
-            "Activate Basic Redirect Test callback handler", 
-            event_type="Receive", 
-            event_key="02V0P37Dz09zR3fL"
+            "Activate Basic Redirect Test callback handler",
+            event_type="Receive",
+            event_key="02V0P37Dz09zR3fL",
         )
 
         if "status" in data:
             pass
         else:
             return None
-        
+
         if data["status"] == "Success":
             pass
         else:
             return None
-        
+
         # TODO >>> Maybe implement a response redirect test from here
 
         print("Received redirected data: ", data)
-        
+
         System_Status(path="Logs").change_unit_status(Unit="Client1", Status=False)
 
         return None
 
-class MyClient:
 
-    def __init__ (self, debug_level):
+class MyClient:
+    def __init__(self, debug_level):
         self.debug_level = debug_level
 
     def initializer(self):
-
-        mys_client = MysceliumClient(client_uid="some_client_id", buffer_path="Temp/Client1Data/", log_level=self.debug_level)
+        mys_client = MysceliumClient(
+            client_uid="some_client_id",
+            buffer_path="Temp/Client1Data/",
+            log_level=self.debug_level,
+        )
 
         self.mys_client = mys_client
-        	
+
         receivers = Receivers()
 
         callbacks = [
@@ -118,26 +126,26 @@ class MyClient:
         mys_client.set_workers_num(n_workers=2)
 
         System_Status(path="Logs").change_unit_status(Unit="Client1", Status=True)
-        
+
         mys_client.initialize_client("127.0.0.1", 4444)
 
-        return 
-    
+        return
+
     def monitor_stop_event(self):
-        
-        time.sleep(25) # needs to be a little more to wait to client 2 initialize
+        time.sleep(25)  # needs to be a little more to wait to client 2 initialize
 
         System_Status(path="Logs").change_unit_status(Unit="Client1", Status=True)
 
         while True:
-
             client_status = System_Status(path="Logs").get_unit_status(Unit="Client1")
             host_status = System_Status(path="Logs").get_unit_status(Unit="Host")
 
             if (not client_status) or (not host_status):
                 print("Receive order to stop client 1")
                 System_Status(path="Logs").change_unit_status(Unit="Host", Status=False)
-                System_Status(path="Logs").change_unit_status(Unit="Client1", Status=False)
+                System_Status(path="Logs").change_unit_status(
+                    Unit="Client1", Status=False
+                )
                 break
             else:
                 time.sleep(5)
@@ -146,7 +154,6 @@ class MyClient:
         return
 
     def run(self):
-
         t1 = Process(target=self.initializer, args=())
 
         # t2 = Process(target=senders.send_some_data, args=())
@@ -158,7 +165,7 @@ class MyClient:
         t3.start()
 
         # t2.join()
-        t3.join()  
+        t3.join()
 
         time.sleep(5)
 
@@ -169,6 +176,3 @@ class MyClient:
         t1.join()  # Wait for the process to finish
 
         return
-
-
-
--- Myscelium/tests/test_redirect/client_2_module.py
+++ Myscelium/tests/test_redirect/client_2_module.py
@@ -10,53 +10,71 @@ from ..Logs.test_logs_manager import Events_Manager, System_Status
 
 CLIENT_ID = "randomsclientids"
 
-class Senders:
 
+class Senders:
     @staticmethod
     def send_some_data_to_redirect():
-
         time.sleep(15)
-                
+
         Events_Manager(Unit="Client2", path="Logs").Set_Event(
-            "Try To Schedule Data To Redirect", 
-            event_type="Default", 
+            "Try To Schedule Data To Redirect",
+            event_type="Default",
         )
-        
+
         print("Try to schedule!1")
 
-        
         try:
-            mys_client = MysceliumClient(client_uid=CLIENT_ID, buffer_path="Temp/Client2Data/")
+            mys_client = MysceliumClient(
+                client_uid=CLIENT_ID, buffer_path="Temp/Client2Data/"
+            )
             mys_client.running = True
+
+            max_attempts = 10
+            attemtps = 0
+            while not mys_client.is_client_ready():
+                time.sleep(1)
+                attemtps += 1
+                if attemtps >= max_attempts:
+                    assert False, "Take too long to client be ready"
+                continue
+
+            TARGET_KEY = "test_redirect_handler"
+
+            target_attempts = 0
+            while not mys_client.is_target_ready(TARGET_KEY):
+                if target_attempts >= 5:  # 25s
+                    assert False, "Take too long to target be ready!"
+                print("target isn't ready, so trying again in 5 secs")
+                time.sleep(5)
+                target_attempts += 1
+                continue
 
             command = client_patterns.command_pattern(
                 CLIENT_ID,
-                "test_redirect_handler", 
-                target_key="some_client_id", # This is part of the smart redirect mechanism to redirect commands
-                kwargs={"data": 8}
+                TARGET_KEY,
+                target_key="some_client_id",  # This is part of the smart redirect mechanism to redirect commands
+                kwargs={"data": 8},
             )
 
             result = mys_client.send(command, priority=10)
         except e as e:
             Events_Manager(Unit="Client2", path="Logs").Set_Event(
-            f"{e}", 
-            event_type="Exception", 
-        )
-
+                f"{e}",
+                event_type="Exception",
+            )
 
         Events_Manager(Unit="Client2", path="Logs").Set_Event(
-            "Data To Redirect Scheduled", 
-            event_type="Send", 
-            event_key="02V0P37Dz09zR3fL"
+            "Data To Redirect Scheduled",
+            event_type="Send",
+            event_key="02V0P37Dz09zR3fL",
         )
-        
+
         print(result)
 
+
 class Receivers:
-
     @staticmethod
-    def test_handler(data:str):
-
+    def test_handler(data: str):
         Events_Manager(Unit="Client2", path="Logs").Set_Event(
             "Activate Basic Response Test callback handler"
         )
@@ -65,7 +83,7 @@ class Receivers:
             pass
         else:
             return None
-        
+
         if data["status"] == "Success":
             pass
         else:
@@ -74,17 +92,20 @@ class Receivers:
         print("Received data: ", data)
 
         # time.sleep(5)
-        
+
         # System_Status(path="Logs").change_unit_status(Unit="Client2", Status=False)
 
-class MyClient:
 
-    def __init__ (self, debug_level):
+class MyClient:
+    def __init__(self, debug_level):
         self.debug_level = debug_level
 
     def initializer(self):
-
-        mys_client = MysceliumClient(client_uid=CLIENT_ID, buffer_path="Temp/Client2Data/", log_level=self.debug_level)
+        mys_client = MysceliumClient(
+            client_uid=CLIENT_ID,
+            buffer_path="Temp/Client2Data/",
+            log_level=self.debug_level,
+        )
 
         self.mys_client = mys_client
 
@@ -93,34 +114,31 @@ class MyClient:
         callbacks = [
             client_patterns.callback_pattern(callback=receivers.test_handler),
         ]
-        
+
         mys_client.set_callbacks(callbacks=callbacks)
         mys_client.set_workers_num(n_workers=2)
 
         System_Status(path="Logs").change_unit_status(Unit="Client2", Status=True)
-        
+
         mys_client.initialize_client("127.0.0.1", 4444)
 
-        return 
-    
-    def monitor_stop_event(self):
+        return
 
-        System_Status(path="Logs").change_unit_status(
-            Unit="Client2", 
-            Status=True
-        )
-        
+    def monitor_stop_event(self):
+        System_Status(path="Logs").change_unit_status(Unit="Client2", Status=True)
+
         time.sleep(30)
 
         while True:
-
             client_status = System_Status(path="Logs").get_unit_status(Unit="Client1")
             host_status = System_Status(path="Logs").get_unit_status(Unit="Host")
 
             if (not client_status) or (not host_status):
                 print("Receive order to stop client 2")
                 System_Status(path="Logs").change_unit_status(Unit="Host", Status=False)
-                System_Status(path="Logs").change_unit_status(Unit="Client2", Status=False)
+                System_Status(path="Logs").change_unit_status(
+                    Unit="Client2", Status=False
+                )
                 break
             else:
                 time.sleep(5)
@@ -128,9 +146,8 @@ class MyClient:
 
         return
 
-    def run(self):  
-
-        senders = Senders ()
+    def run(self):
+        senders = Senders()
 
         t1 = Process(target=self.initializer, args=())
         t2 = Process(target=senders.send_some_data_to_redirect, args=())
@@ -139,12 +156,11 @@ class MyClient:
         t1.start()
 
         t2.start()
-        
+
         t3.start()
 
-
         t2.join()
-        t3.join()  
+        t3.join()
 
         time.sleep(5)
 
@@ -155,6 +171,3 @@ class MyClient:
         t1.join()  # Wait for the process to finish
 
         return
-
-
-
