3c36632e010a159b094f66c069a5cb77d75210e3
Cristian Camargo <ccf@cdone.com.br>
2024-03-15 14:17:44 -0300

    Add a log navigator in streamlit to allow better debug

--- Myscelium/tests/interface.py
+++ Myscelium/tests/interface.py
@@ -9,6 +9,12 @@ import pytest
 import xml.etree.ElementTree as ET
 import os
 
+import json
+
+
+# Define the root directory where the Temp folders are located
+TEMP_DIR = os.path.join(os.path.dirname(__file__), "Temp")
+
 # Set the page config to use the full width
 st.set_page_config(layout='wide')
 
@@ -23,7 +29,7 @@ df = df.dropna(subset=['Time'])
 
 # Sidebar menu
 st.sidebar.title("Menu")
-option = st.sidebar.selectbox('Choose an Option', ['Test Results Visualization', 'Test Interface'])
+option = st.sidebar.selectbox('Choose an Option', ['Test Results Visualization', 'Test Interface', 'Logs Navigator'])
 
 def test_fn():
     pass
@@ -198,4 +204,112 @@ elif option == 'Test Interface':
     #     st.write('Button 2 was clicked!')
         
     pass
+
+elif option == 'Logs Navigator':
     
+    # Example list of items
+    items = [f"Item {i}" for i in range(100)]  # Adjust the range for more or fewer items
+    
+    
+    def get_last_directory(path):
+        # Strip the trailing slash if it exists
+        path = path.rstrip(os.path.sep)
+
+        # Check if the last component of the path is a directory
+        if os.path.isdir(path):
+            return os.path.basename(path)
+        else:
+            # If the last component is a file, get the directory name
+            return os.path.basename(os.path.dirname(path))
+    
+    all_logs_dict = {}
+    
+    for root, dirs, files in os.walk(TEMP_DIR):
+        for file in files:
+            if file == "logs.txt":
+                with open(os.path.join(root, file), 'r') as log_file:
+                    
+                    last_dir_name = get_last_directory(root)
+                    
+                    logs = []
+                    
+                    for line in log_file:
+                        try:
+                            entry = json.loads(line.strip())
+                            logs.append(entry)
+                        except json.JSONDecodeError as e:
+                            print(f"Error parsing JSON: {e}, in file: {file}, line: {line}")
+                            
+                    if last_dir_name == "Data":    
+                        all_logs_dict["HOST"] = logs
+                    if last_dir_name == "Client1Data":    
+                        all_logs_dict["CLIENT1"] = logs
+                    if last_dir_name == "Client2Data":    
+                        all_logs_dict["CLIENT2"] = logs
+    
+    log_lines_dict = {}
+    for owner, logs in all_logs_dict.items():
+    
+        for log in logs:
+            
+            if log["log_msg"] == "Nothing in the schedule, skipping >>>":
+                continue
+        
+            if log["log_msg"] == "\nSchedule to process:\n[]\n":
+                continue
+            
+            # -> Remove C206 Ping and C207 Pong they are not necessary for this task and will overflow the model
+            if 'C206' not in log["log_msg"] and 'C207' not in log["log_msg"]:
+                pass
+            else:
+                continue
+    
+            if log["log_time"] == "":
+                continue
+
+            log_lines_dict[log["log_time"]] = f"{owner}: " + log["log_msg"] + "\n"
+
+    # Sorting the dictionary by its keys (timestamps)
+    sorted_dict = {k: log_lines_dict[k] for k in sorted(log_lines_dict)}  
+                            
+                            
+    # # Example list of strings
+    # log_entries = ["INFO: This is an info message",
+    #             "WARNING: This is a warning message",
+    #             "ERROR: This is an error message",
+    #             "INFO: Another info message"]
+
+    # Color mapping for each label
+    # color_map = {
+    #     "INFO": "green",
+    #     "WARNING": "orange",
+    #     "ERROR": "red"
+    # }
+
+    # Color mapping for each label
+    color_map = {
+        "CLIENT1": "green",
+        "CLIENT2": "orange",
+        "HOST": "lightblue"
+    }
+
+
+    # Create an expander
+    with st.expander("Log Entries", expanded=True):
+        # Generate HTML with different colors for each log entry
+        log_html = ""
+        for entry in sorted_dict.values():
+            label, content = entry.split(": ", 1)
+            color = color_map.get(label, "black")  # Default to black if label is not in color_map
+            log_html += f'<p style="color: {color};">{label}: {content}</p>'
+
+        st.markdown(log_html, unsafe_allow_html=True)                        
+                        
+    
+#    # Create an expander
+#     with st.expander("Scrollable List", expanded=True):
+#         # Use markdown or HTML for a more compact layout
+#         items_str = '\n'.join(f'- {item}' for item in sorted_dict.values())
+#         st.markdown(items_str)
+        
+    pass
