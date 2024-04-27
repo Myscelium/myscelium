2d80342a43269c891cb0f98e5e9117bd1829f5cd
Cristian Camargo <ccf@cdone.com.br>
2024-04-27 16:47:40 -0300

    Better organize some functions and patterns in a dedicated dir to reduce the amount of code in the wrapper

--- Myscelium/myscelium/__init__.py
+++ Myscelium/myscelium/__init__.py
@@ -1,4 +1,6 @@
-from .wrapper import MysceliumClient, ClientPatterns, MysceliumClientInterface, MysceliumHost, HostPatterns, MysceliumHostInterface, CallbackCollector, GetHostClients, callback_pattern, HostConfigManager
+from .wrapper import MysceliumClient, ClientPatterns, MysceliumClientInterface, MysceliumHost, HostPatterns, MysceliumHostInterface, GetHostClients, HostConfigManager
 
 from .common.patterns import ClientPattern
 from .common.patterns import CommandInstruction
+
+from .common.functions import callback_pattern, CallbackCollector
--- Myscelium/myscelium/wrapper.py
+++ Myscelium/myscelium/wrapper.py
@@ -26,133 +26,7 @@ from .common.patterns import CommandInstruction
 
 # > Type Cast
 
-def cast_response_command_instruction(
-    command_mode: str,
-    command_type: str,
-    command_target: str,
-    command_status: str,
-    command_origin: str,
-    command_actf: str,
-    command_kwargs: dict,
-    command_message: str,
-    auto_collect: bool = True,
-) -> dict:
-    """
-    Constructs a command instruction dictionary from the given parameters.
-
-    Validates the provided arguments against specific criteria for each command aspect
-    (mode, type, target, status, origin, activation function). Raises an exception if
-    any of the provided arguments do not meet the expected values or format.
-
-    Parameters:
-    - command_mode (str): Mode of the command, must be one of ['Function', 'Response'].
-    - command_type (str): Type of the command, must be one of ['SpecialFunction', 'DirectFunction',
-                      'InternalManagement', 'ExternalFunction'].
-    - command_target (str): Target of the command, should follow the format 'Origin',
-                            'ClientKey(String)', or 'Host'.
-    - command_status (str): Status of the command, must be one of ['Success', 'Failure'].
-    - command_origin (str): Origin of the command, must be 'Host' or 'ClientKey(String)'.
-    - command_actf (str): Activation function for the command. Cannot be empty.
-    - command_kwargs (dict): Additional keyword arguments for the command.
-    - command_message (str): Message associated with the command.
-
-    Returns:
-    dict: A dictionary representing the constructed command instruction.
-
-    Raises:
-    Exception: If any parameter does not conform to its expected format or allowable values.
-
-    Example:
-    command_dict = cast_command_instruction("Function", "DirectFunction", "Host", "Success",
-                                            "ClientKey(123)", "activate", {}, "Execute action")
-    """
-
-    if command_mode not in ["Function", "Response"]:
-        raise ValueError(
-            "Command mode needs to be one of those: ['Function', 'Response']"
-        )
-
-    if command_type not in [
-        "SpecialFunction",
-        "DirectFunction",
-        "InternalManagement",
-        "ExternalFunction",
-    ]:
-        raise ValueError(
-            "Command type needs to be one of those: ['SpecialFunction', 'DirectFunction', 'InternalManagement', 'ExternalFunction',]"
-        )
-
-    if command_target in ["Origin", "Host"]:
-        pass
-
-    # -> Validate the redirect cases:
-    elif command_target.startswith("ClientKey(") and command_target.endswith(")"):
-        # Extracting the part inside 'ClientKey()'
-        content = command_target[len("ClientKey(") : -1].strip()
-        
-        # Validate the content inside the parentheses
-        if content == "":
-            raise ValueError("Command target ClientKey needs a valid ClientKey!")
-        
-        command_target = content
-        
-
-    else:
-        raise ValueError(
-            "Command target must be either 'Origin', 'Host', or 'ClientKey(some_value)'"
-        )
-
-    if command_status not in ["Success", "Failure"]:
-        raise ValueError(
-            "Command status can only be one of those: ['Success', 'Failure']"
-        )
-
-    if command_origin == "Host":
-        pass
-
-    # -> Validate the client cases:
-    elif command_origin.startswith("ClientKey(") and command_origin.endswith(")"):
-        # Extracting the part inside 'ClientKey()'
-        content = command_origin[len("ClientKey(") : -1].strip()
-        
-        # Validate the content inside the parentheses
-        if content == "":
-            raise ValueError("Command target ClientKey needs a valid ClientKey!")
-        
-        command_origin = content
-
-    else:
-        raise ValueError(
-            "Command origin must be either 'Host' or 'ClientKey(some_value)'"
-        )
-
-    if (
-        auto_collect
-    ):  # command_actf definition is only required in cases where auto_collect is on
-        if command_actf == "" or command_actf == None:
-            raise ValueError("Command activation function can't be empty")
-    else:
-        pass
-
-    command_instruction = {
-        "mode": command_mode,
-        "type": command_type,
-        "target": command_target,
-        "status": command_status,
-        "origin": command_origin,
-        "actf": command_actf,
-        "kwargs": command_kwargs,
-        "message": command_message,
-        "response_type": command_type,  # This is a duplication due to a temporary change in the Option downcast
-        "response_target": command_origin,  # This is a duplication due to a temporary change in the Option downcast
-        "response_actf": command_actf,  # This is a duplication due to a temporary change in the Option downcast
-        "collect_response": auto_collect,  # Default here is true, but if can be changed in the command that trigger this handler that send this response, if False it will not be automatically Transposed.
-    }
-
-    # TODO >>> Find a sulution to use None in the above struct casting in the `response_type`, `response_target`, `response_actf` and not a repetition of the act and other fields used to replace None
-
-    return command_instruction
-
+from .common.functions import cast_response_command_instruction
 
 # >-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # > Utilities
@@ -167,17 +41,18 @@ class GetHostClients:
         connection = self.pool.get_connection()
         cur = connection.cursor()
         cur.execute(
-            """CREATE TABLE IF NOT EXISTS Clients (ID INT PRIMARY KEY,
-                                                        ClientName TEXT,
-                                                        ClientKey TEXT,
-                                                        ClientType TEXT,
-                                                        PermissionGroup TEXT,
-                                                        SuperUser BOOL,
-                                                        LastContact FLOAT,
-                                                        MaxSubChannels NUMBER,
-                                                        OwnedSubChannelsKeys TEXT,
-                                                        SubChannelsInUse NUMBER
-        )"""
+            """CREATE TABLE IF NOT EXISTS Clients (
+                ID INT PRIMARY KEY,
+                ClientName TEXT,
+                ClientKey TEXT,
+                ClientType TEXT,
+                PermissionGroup TEXT,
+                SuperUser BOOL,
+                LastContact FLOAT,
+                MaxSubChannels NUMBER,
+                OwnedSubChannelsKeys TEXT,
+                SubChannelsInUse NUMBER
+            )"""
         )
 
         self.pool.release_connection(connection)
@@ -214,138 +89,6 @@ class GetHostClients:
         dict_df = df.to_dict()
 
         return dict_df
-
-
-import inspect
-
-# > ----------------------------------------------------------------------------------------------------------------------------------------------
-# > Callbacks
-
-
-def callback_pattern(callback) -> dict:
-    """
-    Create a callback pattern.
-
-    Parameters:
-    - callback: The callback function.
-
-    args and kwargs: Will be auto inferred by the wrapper, just add the notes to your functions.
-
-    Returns:
-    - Dictionary representing the callback pattern.
-    """
-
-    # > The idea of this pattern
-    # >
-    # > (Client 1)         [Host]
-    # >    |                 |
-    # >    | --------------> |
-    # >    |                [|]
-    # >   (|) <------------- |
-    # >    |                 |
-    # > (Client 1)         [Host]
-    # >
-    # > (|) This is this callback
-    # > [|] This is a host process
-    # >
-    # > Basically this creates a callable, that host can execute remotely by redirecting some command or sending some command
-
-    sig = inspect.signature(callback)
-    params = sig.parameters
-
-    args = {}
-
-    for name, param in params.items():
-        if param.annotation is inspect._empty:
-            function_name = callback.__name__
-            raise f"function: {function_name} has args or kwargs without the required notes!"
-        else:
-            pass
-
-        args[name] = str(param.annotation.__name__)
-
-    else:
-        pass
-
-    if "info" not in args:
-        function_name = callback.__name__
-        raise ValueError(
-            f"Fist argument must be info, ifnor is a carrier argument designed to send crutial info about then engine and general staus, you must define it inside function: {function_name}"
-        )
-    else:
-        pass
-
-    args.pop(
-        "info", None
-    )  # Remove the info carrier arg in the start of the callback argument
-
-    # Info carrier argument is only defined in the function as a delimiter to the
-    # information that will be sended by the engine to the callback, it isn't a
-    # requirement for remote call this argument, however this is necessary to delimit
-    # that space and say that the first arg is the info carrier. The engine know that
-    # too so it isn't required to be sended to inside the engine.
-
-    callback_pattern = {
-        "function": callback,
-        "args": args,
-    }
-
-    return callback_pattern
-
-class CallbackCollector:
-    """
-    Extract from a list of classes (Handlers, Receivers and Retransmiters) the methods and callbacks in it
-    And automatically creates the callback list, simplifiing even more the process of create new callbacks and
-    Methods for you host or to you client.
-
-    IMPORTANT!: This only extract static methods toa llow you to use other methods inside your class
-
-    Usage:
-
-    ```
-    callbacks_list = CallbackCollector([Handlers, Receivers, Retransmiters]).get_callbacks()
-    ```
-    """
-
-    def __init__(self, callback_containers):
-        self.callbacks = []
-        for container in callback_containers:
-            self._get_methods(container)
-
-    def _get_methods(self, callback_class):
-        # Get all attributes of Class
-        for name, obj in inspect.getmembers(callback_class):
-            # Check if it is a function/method
-            if (
-                inspect.isfunction(obj)
-                or inspect.ismethod(obj)
-                or isinstance(obj, staticmethod)
-            ):
-                # If it's a static method, get the underlying function
-                if isinstance(obj, staticmethod):
-                    obj = obj.__get__(None, None)
-
-                # Check if obj is not None before proceeding
-                if obj is not None:
-                    callback_pattern_result = callback_pattern(callback=obj)
-                    # Check if callback_pattern_result is not None before appending
-                    if callback_pattern_result is not None:
-                        self.callbacks.append(callback_pattern_result)
-
-    def get_callbacks(self):
-        """
-        Extract from a list of classes (Handlers, Receivers and Retransmiters) the methods and callbacks in it
-        And automatically creates the callback list, simplifiing even more the process of create new callbacks and
-        Methods for you host or to you client.
-
-        Usage:
-
-        ```
-        callbacks_list = CallbackCollector([Handlers, Receivers, Retransmiters]).get_callbacks()
-        ```
-        """
-        return self.callbacks
-
 
 # >-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # > HOST
