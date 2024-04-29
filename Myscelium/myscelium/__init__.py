d5cc0a388e9565d5b04685c931e2c837d507df12
Cristian Camargo <ccf@cdone.com.br>
2024-04-29 18:05:23 -0300

    Moved some of the client interfaces to their own file turning the wrapper eveen more organized

--- Myscelium/myscelium/__init__.py
+++ Myscelium/myscelium/__init__.py
@@ -1,4 +1,4 @@
-from .wrapper import MysceliumClient, ClientPatterns, MysceliumClientInterface, MysceliumHost, HostPatterns, HostConfigManager
+from .wrapper import MysceliumClient, ClientPatterns, MysceliumHost, HostPatterns, HostConfigManager
 
 from .common.patterns import ClientPattern
 from .common.patterns import CommandInstruction
@@ -7,3 +7,4 @@ from .common.functions import callback_pattern
 from .common.utilities import CallbackCollector
 
 from .server.interfaces import GetHostClients, MysceliumHostInterface
+from .client.interfaces import MysceliumClientInterface
--- Myscelium/myscelium/wrapper.py
+++ Myscelium/myscelium/wrapper.py
@@ -4,26 +4,21 @@ from . import (
     myscelium_engine as mys,
 )  # Maybe change the rust myscelium lib to MysceliumEngine
 
-#> Client:
-from .client import client_logs_retriever
-from .common.logs_transposition import transpose, check_if_all_logs_was_transposed
-
 #> Modules:
-from .common.custom_decorators import experimental, stable
-from .common import sql_pool
 from .common.patterns import ClientPattern
 from .common.patterns import CommandInstruction
 from .common.functions import cast_response_command_instruction
-from .common.functions import split_dataframe
 
 #> Extern:
 import functools
 import warnings
-from multiprocessing import Process
 import pandas as pd
 import time
 import os
 from typing import Dict
+
+from .server.interfaces import MysceliumHostInterface
+from .client.interfaces import MysceliumClientInterface
 
 # >-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # > HOST
@@ -698,150 +693,6 @@ class MysceliumClient:
         - parity id: this parity id is a unique string assigned to to command when sending, used to sincronize the command and response between the async system
         """
         return mys.wait_client_resp(parity_id, timeout_in)
-
-
-class MysceliumClientInterface:
-    def __init__(self, buffer_path: str) -> None:
-        """
-        Initialize the MysceliumHostInterface.
-
-        Parameters:
-        - buffer_path: Path to the buffer for logs retrieval.
-        """
-
-        self.buffer_path = buffer_path
-
-        self.log_callback = ""
-
-        self.stats = False
-
-        self.process = ""
-
-        self.transposition_threads = 1
-
-        return
-
-    def retrieve_logs(self):
-        """
-        Retrieve logs and process them. If multiple threads are set, it will split the logs
-        and process them in parallel.
-        """
-
-        pool = sql_pool.SQLiteConnectionPool(
-            self.transposition_threads + 2, os.path.join(self.buffer_path, "Logs.db")
-        )
-
-        connection = pool.get_connection()
-
-        logs_retriever_access = client_logs_retriever.Logs_Buffer_retriever(connection)
-
-        while True:
-            if not self.stats:
-                while True:
-                    if check_if_all_logs_was_transposed:
-                        break
-                    else:
-                        continue
-
-                break
-
-            else:
-                pass
-
-            logs_dict_df = logs_retriever_access.List_Logs()
-            logs_df = pd.DataFrame.from_dict(logs_dict_df)
-
-            if logs_df.empty:
-                time.sleep(2)
-                continue
-            else:
-                pass
-
-            logs_df = logs_df.sort_values("LogTime")
-            logs_df = logs_df.reset_index(drop=True)
-
-            if self.transposition_threads > 1:
-                logs_df_chunks = split_dataframe(logs_df, self.transposition_threads)
-
-                threads = []
-
-                for chunk in logs_df_chunks:
-                    threads.append(
-                        Process(
-                            target=transpose,
-                            args=(chunk, self.buffer_path, self.log_callback),
-                        )
-                    )
-                    continue
-
-                for t in threads:
-                    t.start()
-                    continue
-
-                for t in threads:
-                    t.join()
-                    continue
-
-                pass
-
-            else:
-                transpose(logs_df, self.buffer_path, self.log_callback)
-                pass
-
-            time.sleep(1)
-
-            continue
-
-        pool.release_connection(connection)
-
-        return
-
-    def allow_multi_handlers(self, workers_num=2):
-        """
-        Activate multiple handlers for processing logs.
-
-        Parameters:
-        - threads_num: Number of threads to be used for processing logs.
-        """
-
-        self.transposition_threads = workers_num
-
-        return
-
-    def set_logs_callback(self, callback: str):
-        """
-        Set the callback function for logs.
-
-        Parameters:
-        - callback: Callback function to be invoked for each log.
-        """
-
-        self.log_callback = callback
-
-        pass
-
-    def stop_logs_retriever(self):
-        """
-        Stop the logs retriever process.
-        """
-
-        self.stats = False
-        self.process.join()
-
-        return
-
-    def start_logs_retriever(self):
-        """
-        Start the logs retriever process in a separate process.
-        """
-
-        self.stats = True
-
-        self.process = Process(target=self.retrieve_logs, args=())
-        self.process.start()
-
-        return
-
 
 class ClientPatterns:
     def __init__(self) -> None:
