614da26b46b7c1f103056479ce59e7ce5eabbc20
Cristian Camargo <ccf@cdone.com.br>
2026-08-30 17:09:46 -0300

    chore: add MPL-2.0 source headers

--- Myscelium/myscelium/common/functions.py
+++ Myscelium/myscelium/common/functions.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright © 2021-2026 Cristian Camargo Filho
+
 def cast_response_command_instruction(
     command_mode: str,
     command_type: str,
--- Myscelium/myscelium/common/patterns.py
+++ Myscelium/myscelium/common/patterns.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright © 2021-2026 Cristian Camargo Filho
+
 from pydantic import BaseModel, Field
 from pydantic import BaseModel, field_validator, model_validator
 from typing import List, Dict, Any, Optional
