7360d860aef82e5b22a3d57ccfab037fa4984464
Cristian Camargo <ccf@cdone.com.br>
2026-08-30 17:09:53 -0300

    chore: add MPL-2.0 source headers

--- Myscelium/build/lib.win-amd64-cpython-310/myscelium/__init__.py
+++ Myscelium/build/lib.win-amd64-cpython-310/myscelium/__init__.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright © 2021-2026 Cristian Camargo Filho
+
 from .wrapper import MysceliumClient, ClientPatterns, MysceliumClientInterface, MysceliumHost, HostPatterns, HostConfigManager
 
 from .common.patterns import ClientPattern
