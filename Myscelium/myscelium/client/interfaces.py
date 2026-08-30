b308c1d28328bcdef09340d1f1e09a6082b7a943
Cristian Camargo <ccf@cdone.com.br>
2026-08-30 17:08:50 -0300

    chore: add MPL-2.0 source headers

--- Myscelium/myscelium/client/interfaces.py
+++ Myscelium/myscelium/client/interfaces.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright © 2021-2026 Cristian Camargo Filho
+
 from ..common import sql_pool
 from ..client import client_logs_retriever
 from ..common.functions import split_dataframe
