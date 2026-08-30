a54d1903dccff39cabb294dc2d9bd45ab544b213
Cristian Camargo <ccf@cdone.com.br>
2026-08-30 17:10:00 -0300

    chore: add MPL-2.0 source headers

--- Myscelium/tests/ManualTests/test_connection/host_module.py
+++ Myscelium/tests/ManualTests/test_connection/host_module.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright © 2021-2026 Cristian Camargo Filho
+
 from myscelium import MysceliumHost, HostPatterns, MysceliumHostInterface
 from multiprocessing import Process, Event, Manager
 import os
